4cf8eaebd0ef9b309699d11be67e8f9e7faea1f2
jsygrl <90338013+jsygrl@users.noreply.github.com>
2021-09-10 15:43:15 +0100

    update

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 // grab a reference for necessary HTML elements
-
 // .joke-text
-const jokeText = document.querySelector('.joketext');
-// .new-joke-btn
+const jokeText = document.querySelector('.joke-text');
+// .new-joke-btn 
 const newJokeBtn = document.querySelector('.new-joke-btn');
-// .tweet-btn (link)
-const tweetBtn = document.querySelector('.tweet-btn');
-// add 'click' eventListener to .new-joke-btn
 
+// add 'click' eventListener to .new-joke-btn
 newJokeBtn.addEventListener('click', getJoke);
 
 // immediately call getJoke()
 getJoke();
 
 // getJoke() function definition
-function getJoke(){
-    // make an API request to https://icanhazdadjoke.com/'
-    fetch ('https://icanhazdadjoke.com/', {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(function(response) {
-        /* convert Stringified JSON response to Javascript Object */
-        return response .json();
-    }).then(function(data) {
-        /* replace innerText of .joke-text with data.joke */
-        // extract the joke text
-        const joke = data.joke;
-        // do the replacement
-        jokeText.innerText = joke;
+function getJoke() {
+  // make an API request to https://icanhazdadjoke.com/'
+  fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(function(response) {
+    /* convert Stringified JSON response to Javascript Object */
+    return response.json();
+  }).then(function(data) {
+    /* replace innerText of .joke-text with data.joke */
+    // extract the joke text
+    const joke = data.joke;
+    // do the replacement
+    jokeText.innerText = joke;
 
-}).catch(function(error) {
+  }).catch(function(error) {
     // if some error occured
-    jokeText.innerText = 'Oops! Someone goofed up!';
+    jokeText.innerText = 'Oops! Some error happened :(';
     // removes the old href from .tweet-btn if found any
     tweetBtn.removeAttribute('href');
     // console log the error
     console.log(error);
-});
-
+  });
 }
